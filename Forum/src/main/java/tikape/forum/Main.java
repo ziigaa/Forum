43f3a1dbb244ca47bb7c6b14c8e9493ae1008748
tuomovii <tuomo.viitaja@helsinki.fi>
2017-02-13 11:46:09 +0200

    tuomon testailu

--- Forum/src/main/java/tikape/forum/Main.java
+++ Forum/src/main/java/tikape/forum/Main.java
@@ -4,14 +4,22 @@
  * and open the template in the editor.
  */
 package tikape.forum;
+import java.util.HashMap;
+import spark.ModelAndView;
 import static spark.Spark.*;
+import spark.template.thymeleaf.ThymeleafTemplateEngine;
 /**
  *
  * @author Joonas <>
  */
 public class Main {
      public static void main(String[] args) throws Exception {
-        get("*", (req, res) -> "Hello World");
+        get("*", (req,res) -> {
+        HashMap<String,String> data = new HashMap();
+        return new ModelAndView(data, "etusivu");
+        }, new ThymeleafTemplateEngine());
+        
+       
 //        get("/salaisuus", (req, res) -> "Kryptos");
 //        get("/path", (req, res) -> "Polku (path)");
 //        get("/route", (req, res) -> "Polku (route)");
