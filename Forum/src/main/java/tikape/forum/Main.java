bd8b40702d6d1735473ae3da6f925d626f597658
Z <z@localhost.localdomain>
2017-02-18 21:13:33 +0200

    asdlol

--- Forum/src/main/java/tikape/forum/Main.java
+++ Forum/src/main/java/tikape/forum/Main.java
@@ -4,48 +4,60 @@
  * and open the template in the editor.
  */
 package tikape.forum;
+
 import java.util.HashMap;
 import spark.ModelAndView;
 import static spark.Spark.*;
 import spark.template.thymeleaf.ThymeleafTemplateEngine;
+
 /**
  *
  * @author Joonas <>
  */
 public class Main {
-     public static void main(String[] args) throws Exception {
-         ThymeleafTemplateEngine engine = new ThymeleafTemplateEngine();
-         
-         get("/", (request, response) -> {
-        HashMap<String,String> data = new HashMap();
-        
-        
-        return new ModelAndView(data, "index");
-    }, engine);
 
-         get("/login", (request, response) -> {
-        HashMap<String,String> data = new HashMap();
+    public static void main(String[] args) throws Exception {
+        ThymeleafTemplateEngine engine = new ThymeleafTemplateEngine();
         
-        
-        return new ModelAndView(data, "login");
-    }, engine);
+        post("/", (req, res) -> {
+            HashMap<String, String> data = new HashMap();
+            if (req.queryParams("newuser") != null) {
+                return new ModelAndView(data, "createuser");
+            } else if (req.queryParams("login") != null) {
+                return new ModelAndView(data, "login");
+            }
 
-   post("/login", (req, res) -> {
-        //System.out.println(req.toString());
-        HashMap<String,String> data = new HashMap();
-        String username = req.queryParams("username");
-        String password = req.queryParams("password");
-        System.out.println(username + " " + password);
-        //System.out.println(request.body());
-        //System.out.println(userResponse.location);
-        //response.redirect("/locationAccepted");
-        return new ModelAndView(data, "login");
-    });
+            return new ModelAndView(data, "login");
+        });
+
+        get("/", (req, res) -> {
+            HashMap<String, String> data = new HashMap();
+
+            return new ModelAndView(data, "index");
+        }, engine);
+
+        get("/login", (req, res) -> {
+            HashMap<String, String> data = new HashMap();
+
+            return new ModelAndView(data, "login");
+        }, engine);
+
+        post("/login", (req, res) -> {
+            //System.out.println(req.toString());
+            HashMap<String, String> data = new HashMap();
+            String username = req.queryParams("username");
+            String password = req.queryParams("password");
+            System.out.println(username + " " + password);
+            //System.out.println(request.body());
+            //System.out.println(userResponse.location);
+            //response.redirect("/locationAccepted");
+            return new ModelAndView(data, "login");
+        });
 
 //        get("/salaisuus", (req, res) -> "Kryptos");
 //        get("/path", (req, res) -> "Polku (path)");
 //        get("/route", (req, res) -> "Polku (route)");
 //        get("/trail", (req, res) -> "Polku (trail)");
     }
-     
+
 }
