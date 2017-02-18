1488625e039af901b5c29f843744c031f097f2e9
Z <z@localhost.localdomain>
2017-02-18 20:52:42 +0200

    lol

--- Forum/src/main/java/tikape/forum/Main.java
+++ Forum/src/main/java/tikape/forum/Main.java
@@ -14,15 +14,38 @@ import spark.template.thymeleaf.ThymeleafTemplateEngine;
  */
 public class Main {
      public static void main(String[] args) throws Exception {
-        get("*", (req,res) -> {
+         ThymeleafTemplateEngine engine = new ThymeleafTemplateEngine();
+         
+         get("/", (request, response) -> {
         HashMap<String,String> data = new HashMap();
-        return new ModelAndView(data, "etusivu");
-        }, new ThymeleafTemplateEngine());
         
-       
+        
+        return new ModelAndView(data, "index");
+    }, engine);
+
+         get("/login", (request, response) -> {
+        HashMap<String,String> data = new HashMap();
+        
+        
+        return new ModelAndView(data, "login");
+    }, engine);
+
+   post("/login", (req, res) -> {
+        //System.out.println(req.toString());
+        HashMap<String,String> data = new HashMap();
+        String username = req.queryParams("username");
+        String password = req.queryParams("password");
+        System.out.println(username + " " + password);
+        //System.out.println(request.body());
+        //System.out.println(userResponse.location);
+        //response.redirect("/locationAccepted");
+        return new ModelAndView(data, "login");
+    });
+
 //        get("/salaisuus", (req, res) -> "Kryptos");
 //        get("/path", (req, res) -> "Polku (path)");
 //        get("/route", (req, res) -> "Polku (route)");
 //        get("/trail", (req, res) -> "Polku (trail)");
     }
+     
 }
